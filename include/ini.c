8e02a6c5d33cc07b95d3967f347a2ea4cc56763d
Stefan Sundin <stefansundin@users.noreply.github.com>
2010-01-07 19:38:06 +0000

    Tagging 1.1 release.

--- include/ini.c
+++ include/ini.c
@@ -18,10 +18,9 @@
 int main(int argc, char *argv[]) {
 	if (argc < 4) {
 		printf("Not enough arguments\n");
-		printf("Usage: ini <file> <section> <key> [new value]\n");
+		printf("Usage: ini <file> <section> <key> [value]\n");
 		return 0;
 	}
-	
 	//Get path
 	char path[MAX_PATH];
 	if (PathIsRelative(argv[1])) {
@@ -32,21 +31,21 @@ int main(int argc, char *argv[]) {
 	else {
 		strcpy(path, argv[1]);
 	}
-	
 	//Write/Read
-	if (argc == 4) {
+	if (argc == 3) {
 		char txt[1000];
-		GetPrivateProfileString(argv[2], argv[3], NULL, txt, sizeof(txt), path); //No error detection possible
+		GetPrivateProfileString(argv[2], argv[3], NULL, txt, sizeof(txt), path); //No error detection
 		printf(txt);
 	}
-	else if (WritePrivateProfileString(argv[2],argv[3],argv[4],path) == 0) {
-		int errorcode = GetLastError();
-		char *errormsg;
-		int length = FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM,NULL,errorcode,0,(char*)&errormsg,0,NULL);
-		errormsg[length-2] = '\0'; //Remove that damn newline at the end of the formatted error message
-		printf("WritePrivateProfileString() failed in file %s, line %d.\nError: %s (%d)", TEXT(__FILE__), __LINE__, errormsg, errorcode);
-		LocalFree(errormsg);
-		return 1;
+	else {
+		if (WritePrivateProfileString(argv[2],argv[3],argv[4],path) == 0) {
+			int errorcode = GetLastError();
+			char errormsg[100];
+			FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, errorcode, 0, errormsg, sizeof(errormsg), NULL);
+			errormsg[strlen(errormsg)-2] = '\0'; //Remove that damn newline at the end of the formatted error message
+			printf("WritePrivateProfileString() failed in file %s, line %d.\nError: %s (%d)", TEXT(__FILE__), __LINE__, errormsg, errorcode);
+			return 1;
+		}
 	}
 	return 0;
 }
